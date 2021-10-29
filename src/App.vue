271ded866d2f8954861e34c71ad9d63088eaf4ee
Italo Canturil <italo.cantur@gmail.com>
2021-10-28 22:20:10 -0300

    Controlando rotas autenticadas

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,34 @@
 </template>
 
 <script>
+import { watch } from '@vue/runtime-core'
+import { useRoute, useRouter } from 'vue-router'
 import ModalFactory from './components/ModalFactory'
-
+import services from './services'
 export default {
-  components: { ModalFactory }
+  components: { ModalFactory },
+  setup () {
+    const router = useRouter()
+    const route = useRoute()
+
+    /*  função watch recebe dois parametros
+      O primeiro é uma função que retorna o que vai ser escutado
+      E quando mudar vai ser chamado a segunda função */
+    watch(() => route.path, async () => {
+      if (route.meta.hasAuth) {
+        const token = window.localStorage.getItem('token')
+        if (!token) {
+          router.push({ name: 'Home' })
+          return
+        }
+        const { data } = await services.users.getMe()
+        console.log(data)
+      }
+    })
+
+    return {
+      router
+    }
+  }
 }
 </script>
