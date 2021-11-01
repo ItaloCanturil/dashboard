7241c789aa0fec87ac5fc6ed71c0b102af1eadad
Italo Canturil <italo.cantur@gmail.com>
2021-10-31 23:09:17 -0300

    Add global loading for req/res

--- src/services/index.js
+++ src/services/index.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import router from '../router'
 import AuthService from './auth'
 import UsersService from './users'
+import { setGlobalLoading } from '../store/global'
 
 const API_ENVS = {
   local: 'http://localhost:3000'
@@ -13,6 +14,7 @@ const httpClient = axios.create({
 // interceptor é uma função que fica no meio da requisição, viajando,
 // pode ser response ou request
 httpClient.interceptors.request.use(config => {
+  setGlobalLoading(true)
   const token = window.localStorage.getItem('token')
 
   if (token) {
@@ -22,16 +24,22 @@ httpClient.interceptors.request.use(config => {
   return config
 })
 
-httpClient.interceptors.response.use((response) => response, (error) => {
+httpClient.interceptors.response.use((response) => {
+  setGlobalLoading(false)
+  return response
+}, (error) => {
   const canThrowAnError = error.request.status === 0 || error.request.status === 500
 
   if (canThrowAnError) {
+    setGlobalLoading(false)
     throw new Error(error.message)
   }
 
   if (error.response.status === 401) {
     router.push({ name: 'Home' })
   }
+
+  setGlobalLoading(false)
   return error
 })
 
